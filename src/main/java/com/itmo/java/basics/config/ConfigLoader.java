ed33f39a5a655c38c7b4e5aab5536e90717d0d76
Stas <ssugol@yandex.ru>
2021-06-15 20:56:28 +0300

    fix ConfigLoader

--- src/main/java/com/itmo/java/basics/config/ConfigLoader.java
+++ src/main/java/com/itmo/java/basics/config/ConfigLoader.java
@@ -1,34 +1,29 @@
 package com.itmo.java.basics.config;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
+import java.net.URL;
 import java.util.Properties;
 
 /**
  * Класс, отвечающий за подгрузку данных из конфигурационного файла формата .properties
  */
 public class ConfigLoader {
-    private final Path filePath;
+    private final String fileName;
 
     /**
      * По умолчанию читает из server.properties
      */
     public ConfigLoader() {
-        this.filePath = Path.of(
-                "src" + File.separator +
-                        "main" + File.separator +
-                        "resources" + File.separator +
-                        "server.properties");
+        this.fileName = "server.properties";
     }
 
     /**
      * @param name Имя конфикурационного файла, откуда читать
      */
     public ConfigLoader(String name) {
-        this.filePath = Path.of(name);
+        this.fileName = name;
     }
 
     /**
@@ -39,15 +34,14 @@ public class ConfigLoader {
      * Читаются: "kvs.workingPath", "kvs.host", "kvs.port" (но в конфигурационном файле допустимы и другие проперти)
      */
     public DatabaseServerConfig readConfig() {
-        try (InputStream is = new FileInputStream(filePath.toString())) {
+        URL resource = this.getClass().getResource(fileName);
+        String filePath = resource != null ? resource.getPath() : fileName;
+        try (InputStream is = new FileInputStream(filePath)) {
             Properties properties = new Properties();
             properties.load(is);
-            String workingPath = properties.getProperty("kvs.workingPath") != null ?
-                    properties.getProperty("kvs.workingPath") : DatabaseConfig.DEFAULT_WORKING_PATH;
-            String host = properties.getProperty("kvs.host") != null ?
-                    properties.getProperty("kvs.host") : ServerConfig.DEFAULT_HOST;
-            String port = properties.getProperty("kvs.port") != null ?
-                    properties.getProperty("kvs.port") : String.valueOf(ServerConfig.DEFAULT_PORT);
+            String workingPath = properties.getProperty("kvs.workingPath", DatabaseConfig.DEFAULT_WORKING_PATH);
+            String host = properties.getProperty("kvs.host", ServerConfig.DEFAULT_HOST);
+            String port = properties.getProperty("kvs.port", String.valueOf(ServerConfig.DEFAULT_PORT));
             return DatabaseServerConfig
                     .builder()
                     .serverConfig(new ServerConfig(host, Integer.parseInt(port)))
